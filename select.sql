ec16e7855a423299973211b0dbd38941fecdb2ac
IVONILSON <ivonilsoncardoso@hotmail.com>
2018-09-28 11:32:19 -0300

    cláusula WHERE

---
+++ select.sql
@@ -0,0 +1,20 @@
+/*
+* O comando Select é um dos mais importantes comandos do SQL. O mesmo é amplamente utilizado.
+*/
+/*No exemplo abaixo será executado um Select para buscar o campo "nome", "sexo" e "endereco" na tabela
+* cliente
+*/
+SELECT nome, sexo, endereco FROM cliente;
+
+/*O select from * (asterico) só deverá ser utilizado para fins acadêmicos e nunca para fins de bancos de dados
+*comerciais. Abaixo um exemplo de uso do * (asterisco) com select.
+*/
+
+SELECT * FROM cliente;
+
+/*O comando "show tables" é exclusivo do MYSQL. O mesmo não existe em outros SGDBs.
+/*o comando "select now()" mostra a data e hora do banco de dados*/
+
+
+
+
---
+++ where.sql
@@ -0,0 +1,15 @@
+/*
+* Uilizando filtros (cláusulas).
+*/
+/*Abaixo a sintaxe utilizando a cláusula "WHERE". A cláusula "WHERE" significa ONDE. Portanto,
+* a busca retornará somente os registros definidos nos parâmetros após a cláusula.
+*/
+SELECT nome, endereco FROM cliente WHERE sexo = 'M';
+
+/*Continuar para assunto novo no 6:39*/
+
+
+
+
+
+
